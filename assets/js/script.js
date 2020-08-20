0267a5ad874cfe4e864b78133cd8c82b339e6e4a
Richy Rich <richardflores009@gmail.com>
2020-08-19 18:26:44 -0700

    added styles for the dragover

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,7 +188,7 @@ var dropZoneDraghandler = function (event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
-        //console.dir(taskListEl);
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -207,9 +207,20 @@ var dropTaskHandler = function (event) {
     else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+
+    // added above append so that the background color dissapears before drop is completed
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
 
 };
+
+var dragLeaveHandler = function (event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
@@ -226,4 +237,5 @@ pageContentEl.addEventListener("dragover", dropZoneDraghandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
 
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
